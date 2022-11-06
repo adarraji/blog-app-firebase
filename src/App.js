7828c2e3e0c045507d285172268f010139fe14a9
adarraji <a.darraji@outlook.com>
2022-11-06 10:37:27 -0500

    updated post single post route

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ const router = createBrowserRouter([
         element: <Home />
       },
       {
-        path: "/single",
+        path: "/post:id",
         element: <Single />
       },
       {
