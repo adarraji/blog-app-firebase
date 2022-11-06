005de3ea3e38576ead2859ae684e66c7b8c8c4d6
adarraji <a.darraji@outlook.com>
2022-11-06 10:16:27 -0500

    added router and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import './App.css';
+import "./App.css";
+import Register from "./pages/Register";
+import Login from "./pages/Register";
+// import Home from "./pages/Register";
+// import Single from "./pages/Register";
+// import Write from "./pages/Register";
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home!</div>,
+    errorElement: <h1>404 Not Found</h1>
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <RouterProvider router={router} />
     </div>
   );
 }
