9ad38002199ca185aa9b3e539f744df2caeec70c
adarraji <a.darraji@outlook.com>
2022-11-06 11:13:37 -0500

    updated styles

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -33,13 +33,23 @@ const Form = styled.form`
   background-color: white;
   display: flex;
   flex-direction: column;
-  padding: 10px;
+  padding: 50px;
   width: 200px;
   gap: 20px;
 `;
 
-const Input = styled.input``;
+const Input = styled.input`
+  padding: 10px;
+  border: none;
+  border-bottom: 1px solid gray;
+`;
 
-const Button = styled.button``;
+const Button = styled.button`
+  padding: 10px;
+  border: none;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+`;
 
 export default Login;
