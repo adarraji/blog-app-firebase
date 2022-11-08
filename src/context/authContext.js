392dfebb3082cf7880a570a9b0b8a969b2cea85b
adarraji <a.darraji@outlook.com>
2022-11-08 17:18:21 -0500

    fix

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -9,7 +9,7 @@ export const AuthContextProvider = ({ children }) => {
     );
 
     const login = async (inputs) => {
-        const res = await axios.post(`${process.env.REACT_APP_BASE_URL}/auth/login`, inputs);
+        const res = await axios.post(`${process.env.REACT_APP_BASE_URL}/auth/login`, inputs, { withCredentials: true });
         setCurrentUser(res.data);
     };
 
